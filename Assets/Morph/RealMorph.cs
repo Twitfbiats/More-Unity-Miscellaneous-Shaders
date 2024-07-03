17b132045fda8d07904fe9e326eb73e409ab90db
unknown <hung.nt207608@sis.hust.edu.vn>
2024-07-03 17:37:56 +0700

    .

--- Assets/Morph/RealMorph.cs
+++ Assets/Morph/RealMorph.cs
@@ -8,6 +8,9 @@ public class RealMorph : MonoBehaviour
     public MeshFilter mesh2;
     public Vector3[] vertices1;
     public Vector3[] vertices2;
+    public Vector3[] vertices2AfterScalingAndRotating;
+    public float scalingFactor = 90f;
+    public Vector3 rotationFactor = new Vector3(0, 0, 0);
     public Vector2[] uv2;
     public int[] triangles1;
     public int[] triangles2;
@@ -26,7 +29,18 @@ public class RealMorph : MonoBehaviour
 
     void Start()
     {
+        ScaleVertices2(scalingFactor);
         StoreDataForEachTriangle();
+    }
+
+    public void ScaleVertices2(float scale)
+    {
+        vertices2AfterScalingAndRotating = new Vector3[vertices2.Length];
+        for (int i = 0; i < vertices2.Length; i++)
+        {
+            vertices2AfterScalingAndRotating[i] = Matrix4x4.Scale(new Vector3(scale, scale, scale)).MultiplyPoint3x4(vertices2[i]);
+            vertices2AfterScalingAndRotating[i] = Matrix4x4.Rotate(Quaternion.Euler(rotationFactor)).MultiplyPoint3x4(vertices2AfterScalingAndRotating[i]);
+        }
     }
 
     public void CountTriangleOfBothMesh()
@@ -46,9 +60,9 @@ public class RealMorph : MonoBehaviour
             (
                 new PerTriangleData
                 (
-                    vertices2[triangles2[i]],
-                    vertices2[triangles2[i+1]],
-                    vertices2[triangles2[i+2]],
+                    vertices2AfterScalingAndRotating[triangles2[i]],
+                    vertices2AfterScalingAndRotating[triangles2[i+1]],
+                    vertices2AfterScalingAndRotating[triangles2[i+2]],
                     uv2[triangles2[i]],
                     uv2[triangles2[i+1]],
                     uv2[triangles2[i+2]]
